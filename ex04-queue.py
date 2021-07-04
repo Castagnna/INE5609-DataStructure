4de72728c8d8cbd53f4dfb99aae3bf42bceae192
Daniel CS <56129919+Castagnna@users.noreply.github.com>
2021-07-04 16:54:39 -0300

    Update ex04-queue.py

--- ex04-queue.py
+++ ex04-queue.py
@@ -1,9 +1,9 @@
 
 
 class Element:
-    def __init__(self, value:int, previous = None):
+    def __init__(self, value:int, next = None):
         self.__value = value
-        self.__previous = previous
+        self.__next = next
 
     @property
     def value(self) -> int:
@@ -14,61 +14,72 @@ class Element:
         self.__value = value
 
     @property
-    def previous(self):
-        return self.__previous
+    def next(self):
+        return self.__next
 
-    @previous.setter
-    def previous(self, previous):
-        self.__previous = previous
+    @next.setter
+    def next(self, next):
+        self.__next = next
 
 
-class Stack:
+class Queue:
     def __init__(self, max_length: int=4):
-        self.__top = None
+        self.__head = None
+        self.__tail = None
         self.__count_elements = 0
         self.__max_length = max_length
 
     @property
-    def top(self):
-        return self.__top
+    def head(self):
+        return self.__head
 
-    def push(self, value: int):
-        if self.__count_elements < self.__max_length:
-            self.__count_elements += 1
-            new_element = Element(value=value, previous=self.__top)
-            self.__top = new_element
-            
+    @property
+    def tail(self):
+        return self.__tail
+
+    def enqueue(self, value: int):
+        if self.__count_elements >= self.__max_length:
+            print("queue overflow")
+            return
+        
+        self.__count_elements += 1
+        new_element = Element(value=value, next=None)
+
+        if self.__count_elements == 0:
+            self.__head = new_element
+            self.__tail = new_element
         else:
-            print("stack overflow")
+            self.__tail.next = new_element
+            self.__tail = new_element            
 
-    def pop(self):
+    def dequeue(self):
         if self.__count_elements > 0:
             self.__count_elements -= 1
             poped = self.__top.value
             self.__top = self.__top.previous
             return poped
         else:
-            print("empity stack")
+            print("empity queue")
 
 
-new_stack = Stack(max_length=4)
-print(f"top element: {new_stack.top}")
-new_stack.push(1)
-print(f"top element: {new_stack.top.value}")
-new_stack.push(2)
-print(f"top element: {new_stack.top.value}")
-new_stack.push(3)
-print(f"top element: {new_stack.top.value}")
-new_stack.push(4)
-print(f"top element: {new_stack.top.value}")
-new_stack.push(5)
+new_queue = Queue(max_length=4)
+print(f"top element: {new_queue.top}")
+new_queue.push(1)
+print(f"top element: {new_queue.top.value}")
+new_queue.push(2)
+print(f"top element: {new_queue.top.value}")
+new_queue.push(3)
+print(f"top element: {new_queue.top.value}")
+new_queue.push(4)
+print(f"top element: {new_queue.top.value}")
+new_queue.push(5)
 
-print(f"Poped element: {new_stack.pop()}")
-print(f"top element: {new_stack.top.value}")
-print(f"Poped element: {new_stack.pop()}")
-print(f"top element: {new_stack.top.value}")
-print(f"Poped element: {new_stack.pop()}")
-print(f"top element: {new_stack.top.value}")
-print(f"Poped element: {new_stack.pop()}")
-print(f"top element: {new_stack.top}")
-print(f"Poped element: {new_stack.pop()}")
+print(f"Poped element: {new_queue.pop()}")
+print(f"top element: {new_queue.top.value}")
+print(f"Poped element: {new_queue.pop()}")
+print(f"top element: {new_queue.top.value}")
+print(f"Poped element: {new_queue.pop()}")
+print(f"top element: {new_queue.top.value}")
+print(f"Poped element: {new_queue.pop()}")
+print(f"top element: {new_queue.top}")
+print(f"Poped element: {new_queue.pop()}")
