a4500a21dd102b1c09c54ce973387af72bdb2d87
Daniel CS <56129919+Castagnna@users.noreply.github.com>
2021-07-04 15:16:50 -0300

    ex3 stack

---
+++ ex03-stack.py
@@ -0,0 +1,70 @@
+
+
+class Element:
+    def __init__(self, value:int, previous = None):
+        self.__value = value
+        self.__previous = previous
+
+    @property
+    def value(self) -> int:
+        return self.__value
+
+    @value.setter
+    def value(self, value:int):
+        self.__value = value
+
+    @property
+    def previous(self):
+        return self.__previous
+
+    @previous.setter
+    def previous(self, previous):
+        self.__previous = previous
+
+
+class Stack:
+    def __init__(self, max_length: int=4):
+        self.__top = None
+        self.__count_elements = 0
+        self.__max_length = max_length
+
+    @property
+    def top(self):
+        return self.__top
+
+    def push(self, value: int):
+        if self.__count_elements < self.__max_length:
+            self.__count_elements += 1
+            new_element = Element(value=value, previous=self.__top)
+            self.__top = new_element
+            
+        else:
+            print("stack overflow")
+
+    def pop(self):
+        if self.__count_elements > 0:
+            self.__count_elements -= 1
+            self.__top = self.__top.previous
+        else:
+            print("empity stack")
+
+
+new_stack = Stack(max_length=4)
+print(f"top element: {new_stack.top}")
+new_stack.push(1)
+print(f"top element: {new_stack.top.value}")
+new_stack.push(2)
+print(f"top element: {new_stack.top.value}")
+new_stack.push(3)
+print(f"top element: {new_stack.top.value}")
+new_stack.push(4)
+print(f"top element: {new_stack.top.value}")
+new_stack.push(5)
+new_stack.pop()
+print(f"top element: {new_stack.top.value}")
+new_stack.pop()
+print(f"top element: {new_stack.top.value}")
+new_stack.pop()
+print(f"top element: {new_stack.top.value}")
+new_stack.pop()
+print(f"top element: {new_stack.top}")
